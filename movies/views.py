438f5d04c1e082116cfc8dc7b5ab676da8a49dc5
IndongLee <kundera@kakao.com>
2019-11-20 16:51:39 +0900

    11200451

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Movie, Genre, Review
 
-# Create your views here.
+
+# 영화 목록
+def index(request):
+    movies = Movie.objects.all()
+    context = {'movies': movies}
+    return render(request, 'movies/index.html', context)
