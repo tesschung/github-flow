6211c4042416a15c716ccd381abffef8d5baadc6
tesschung <geobera0910@naver.com>
2019-11-20 16:34:47 +0900

    11200434

--- pjt10/settings.py
+++ pjt10/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'pjt10.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'pjt10', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
