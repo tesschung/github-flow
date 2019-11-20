587122390c97ed92ae8858b050d42a3fcc73bb7d
tesschung <geobera0910@naver.com>
2019-11-20 16:48:03 +0900

    11200448

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+app_name='accounts'
+
+urlpatterns = [
+    # 유저 목록
+    path('', views.index, name='index'),
+    # 회원가입
+    path('signup/', views.signup, name='signup'),
+    # 로그인
+    path('login/', views.login, name='login'),
+    # 로그아웃
+    path('logout/', views.logout, name='logout'),
+    # 유저 상세보기
+    path('<int:user_pk>/', views.detail, name='detail'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import *
+from django.contrib.auth.models import User
 
-# Create your views here.
+# 유저 목록
+# AttributeError at /accounts/ 남
+def index(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'accounts/index.html', context)
+
+# 회원가입
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect(movies:index)
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+
+# 로그인
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+        return redirect('movies:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+# 로그아웃
+def logout(request):
+    auth_logout(request)
+    return redirect('movies:index')
+
+# 유저 상세보기
+def detail(request):
+    user = get_object_or_404(User, pk=user_pk)
+    context = {
+        'user': user
+    }
+    return render(request, 'accounts/detail.html', context)
